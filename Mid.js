a6404e5bd100c147b3c25b2e08db2febf2fb1e66
rehjmov <97565093+rehjmov@users.noreply.github.com>
2022-11-14 13:19:26 +0400

    Add files via upload

---
+++ Mid.js
@@ -0,0 +1,35 @@
+class Message {
+    constructor(authorName, time, text) {
+        this.authorName = authorName
+        this.time = time   
+        this.text = text
+    }
+
+    toString() {
+         console.log(`${this.authorName} --->  ${this.time}:${this.text}`)
+    }
+}
+
+
+class Messenger {
+
+    constructor() {
+        this.messages = []
+    }
+
+    show_history() {
+        // console.log(this.messages.map(message => message[1] + ' ' + message[0]).join('\n'));
+        this.messages.map(message => message.toString())
+    }
+
+    send(author, text) {
+        let message = new Message(author, text, new Date().getHours() + ':' + new Date().getMinutes())
+        this.messages.push(message)
+    }
+}
+
+
+let messenger = new Messenger()
+messenger.send('Jesse', 'ilk mesaj')
+messenger.send('Walter', 'İkinci mesaj')
+messenger.show_history()
